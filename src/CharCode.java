2b735827f39761e3834c921ebcfa8f40eda5a0e7
Yupo <yupotown@gmail.com>
2014-09-04 02:16:14 +0900

    CharCode クラスを追加

---
+++ src/CharCode.java
@@ -0,0 +1,14 @@
+
+public class CharCode {
+	
+	/**
+	 * 改行コード
+	 */
+	static final String NL = System.getProperty("line.separator");
+	
+	/**
+	 * 半角スペース
+	 */
+	static final char SPACE = ' ';
+
+}
